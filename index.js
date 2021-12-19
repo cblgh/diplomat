11707d45f4bdb7f01a0018c19bfac0f682949ff8
cblgh <cblgh@cblgh.org>
2021-12-19 18:58:24 +0100

    more data, some tweaks

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-const dump = `[{"key":"x3hFNzhFVGtbeKMGSwX2XyAsQJtOtM34gNZRNDp2Z0A0GXWfR2lWVCQjJxZoYWh2Xwc4Px0APiEdSRcCORAHA2slDkojV2VFVCFOCiQ7KCoaHBlxaUsAcWdFJCgVRT8=","body":"#file:///home/alex/Bureau/test-40mm-macro/DSC_3975.jpg","nick":"ladislas","likes":0,"shares":0,"moderate":false,"type":"post","ptime":1639693852695},{"key":"xTliU1w7cnJDcho4VJkKXzKmWqSfszPRYQJEeEU3M0xyKzWneW9oURskBj4CRCcmHCpkXicUfhcVPF03JkU1MAATICUz","body":"#buugi this is Martins sandbox","nick":"guest","likes":0,"shares":0,"moderate":false,"type":"post","ptime":1639687252695},{"key":"5DhWTWBuW0qzeVM4G4RI43ncMqCdvdJZNQVVs3RVem1yYjlzRVdEJSFvFAJbERkSHVIbNBYpSwY8IQU=","body":"#finhack beep boop","nick":"soup","likes":0,"shares":0,"moderate":false,"type":"post","ptime":1639681093393},{"key":"0G5HWTxhZ14/YI0DkYXeSdGUEBh2RzXW0gBYVDjMa+dVEFpTNKJIvy27NFb81YNvBxBtTVANIgpfFkM3JzE9JiNYJw0bLCYvKhgvBTMhY31qKmklQQtdIGRRGGMWKAdSOCwOOSQXIx0TAWAXESVuDjtWO0U1HyMGJ38SElIVPAYKKj0DC2ktfwMZEg==","body":"#buugi !|LAEpA;ofMd_NivNHxv-;0L%M%M%M  !&QmaasmYziqTnYu3BAKZxZoX6SobQjLYG8WjL9ipKR3u2Ki","nick":"guest","likes":3344,"shares":44734,"moderate":false,"type":"post","ptime":-1},{"key":"5DhWTWBuW0qzeVM4dlIu5Uujbt0AtaetrFdDNmCtcG16YjkzRThFJSFvFAJbERkSHVIbNBYpSwY8IQU=","body":"#finhack beep boop","nick":"guest","likes":0,"shares":0,"moderate":false,"type":"post","ptime":-1},{"key":"5DhWTWBuW0qzeVM4dlJDMy2lXKJcyDql2aPaZHYoZJVwYjEzRXhFJSFvFAJbERkSHVIbNBYpSwY8IQU=","body":"#finhack beep boop","nick":"guest","likes":0,"shares":0,"moderate":false,"type":"post","ptime":-1},{"key":"5DhWTWBuW0qzeVM4dlJDM0BzV3IIsVSnGNYymJqO60JyHy/LR3hNJSFvFAJbERkSHVIbNBYpSwY8IQU=","body":"#finhack beep boop","nick":"guest","likes":0,"shares":0,"moderate":false,"type":"post","ptime":-1},{"key":"xhdUNDh/VUg0WC7bPrwVyTqnFpWIz83mJP5XWDJh0kRlZlX2yY9nuOS7RzeVpL7f4QgnDIb8meFHNS9XWFhKPz4CPSux7BVPFCk6OkxcUCMeAQw4ChM8HzExTFgbWSMDGy8FNT8fGVEsExYnLgZOcHEWH0IqGG4=","body":"käärmeen sylissä !&QmWzj2huqtnhpttgy59X7BNPxKseyU9HUnJjww56SyqaY9","nick":"guest","likes":11278,"shares":49886,"moderate":false,"type":"post","ptime":-1},{"key":"xXJ6ej9sHlZNarwEdz3+SWoXRRHCwDeRpEBjS381eFtLNlXmbH1tSkYnDTdVWi0cS0YcFAcX","body":"suburbian selva","nick":"ladislas","likes":0,"shares":0,"moderate":false,"type":"reply","ptime":-1}]`
+const dump = `[{"key":"5XNKSnpOEWKqU85Vh+2sAMecx0Yu7J8Q/0+4a2o5Okd6XTdzTkJqIm4WFAFSDjgHMQQRJ24ULTBWN1xWGwImQA==","body":"new without attachement","nick":"majorllama","likes":0,"shares":0,"moderate":false,"type":"post","ptime":1639763812695},{"key":"xxczTHliAXyBdd1zCj8oskxzZqcQWEuiU/i9t38cPdPTfcMVbH1DsEBzpDGVCLVWpKXeTX4lRRA0Gz4QAlY2JAwZdgYdPnEZI1cLHyQ1Z3IVAXg/HAsFKTAoICJ8O2U6dw==","body":"a valued rug !|LKHU@0NG~pR*_3NG%MWB~pRjIUWB","nick":"majorllama","likes":65516,"shares":0,"moderate":false,"type":"post","ptime":1639758952695},{"key":"xxczTHliAXmBddBzCj8oskxzZqflRhqb7uUtXnEYPdPTfcMVbNRDsEdbdEpn+fyB+J8iTX4lRRA0Gz4QAlY2JAwZdgYdPnEpAhMLHyQ1Z3IVAXg/HAsFKTAoICJwX2U6dw==","body":"a valued rug !|LKHeatNG~pR*_3NG%MWB~pRjE1WB","nick":"majorllama","likes":50448,"shares":0,"moderate":false,"type":"post","ptime":1639758652695},{"key":"+Ho0ZjNfZWuecBdLqPhNhd6RKLGMy3INMfbO9X9DQnQVam0yYkUySncmWF0pHhIVDhkYGBAWJgA=","body":"conely wainbows","nick":"samui","likes":0,"shares":0,"moderate":false,"type":"post","ptime":1639738912695},{"key":"2xdoVVE7UlK9ZOpUrIXJt/nzg7iPgD1BFWqY+XMVUdDDLdkOMH55wNFgJrFx18ITW+UiDQ9DVAAnAh4fdkNFGQY1Cy0FCyZXCA4+GzQCIQAvAjcqBgYfQwEABl9UeDUhcCcfIxcyEDciBSx+FUEZQg8qQncFHWwkM1I+AQhZCiItMmMPDwUKUwMeOzwVUw==","body":"bubbling !&QmcCTdHH6EEiUrXGLGfqRkBnzDGC9g3tv3jyTdxddwUBJc  !|L26RP_of8|WBjIoek9WBMzayoxkB","nick":"majorllama","likes":55040,"shares":0,"moderate":false,"type":"post","ptime":1639711012695},{"key":"zGdZN2NcbgKYUwpki/horMyyB6E/H+oY0SqFal4jbs/eQsRxWgUzzHm03V6pEHkU4DMrZS8wDExMA2tKVik7NAQuH2QMECV6WjcUCRYJNm82OVwqLg8cIiVcLQYTBXsedxkxKjROHBoFH2dwThUKLX1UC14pNWF9AAYoR2w5BndvHyMddg8VZR5BVlE=","body":"gazza !&QmVgft2DiD94VYBAGp5Pu4NhwqfTehAVcHU6NrgR9oPqL2  !|LD7x8Yt603M}-.oLD,RkD,WX-ns,","nick":"majorllama","likes":1605,"shares":0,"moderate":false,"type":"post","ptime":1639707292695},{"key":"2xdoVVE7VVO9ZCtUmuZqgc+QII72Hh6T2kZVS3cFUdjDLdkOMFx5w0eRpeVjzOq6XI/4DjRGRAsiGAQRdkNFGQYECx8YGSsmGRokA3QjBDslDQMOFT0AUCwWKgVaHScfJXxXMSsbNwZtJQpXIkEZQg8qPhE9CWYgF0d5OSxHdBoKLW03KzYBWTgmATEHUw==","body":"priisti !&QmRCfyZEGTQsM2ybwMiEvxyqiiQociVfHf11FXQCU8udcT  !|LNPjDUkB-;oft7WBt7of~qkBWBfP","nick":"majorllama","likes":48512,"shares":0,"moderate":false,"type":"post","ptime":1639707232695},{"key":"zGdZN2NcbjKYUw9ki/horMyyB6HnJmPy1ha6GkIjbsfaQkRxWhczzHoYfmPzDTU+EqikZS8wDERPUGtKVik7MVQPLWc+HzciNjIlPzkFEhARegYmcAEgBjwKFxMGKlQxIx87JFAyFiUBFmFwThUKLQ0yHiADbGQzcSQcARkAdXoDFRwHNhcKOgNkCFE=","body":"gary2 !&QmS7GF1vfVaXShtnKTJw6nB6yMBM3RTCLgzbHxi6EeouE4  !|L4QmFs-3~BoIk[V?I@XT^lOGrsVr","nick":"majorllama","likes":40394,"shares":0,"moderate":false,"type":"post","ptime":1639707112695},{"key":"8XJOR1I+WVTIM5b/eZTFgSS4lEZUOg8SoQ12EXw7zntnAVokRMyQnAeQTbT5bO70ACprVxtHHz5NFxAzJjNJIS83LysAAnApBRc6MC8SOQgwYzAuLQk1TwkAFX0Dfx1RBh8KMyp4NDU5alRyKRw8fTg1Zx0yWTcZbhJ9PwQBMw4ZFnE+ZhcSLHpo","body":"#buugi !&QmX9YyULckT8PdTTQbYnFv9VbEms7dDdDF8X75TKdi5RBJ  !|LRINT^~A?GX9_NtQkqXS%2s.NHt7","nick":"samuimac","likes":16005,"shares":47616,"moderate":false,"type":"post","ptime":1639696012695},{"key":"8XJOR1I+WVXLM5b/eZTFgSS4Xs2EiooDjWZxEXw7TntnAVqUR8yQnAeQTbT5bO70ACprVxtHHz5NFxAzJjNJIS83LysAAnApBRc6MC8SOQgwYzAuLQk1TwkAFX0Dfx1RBh8KMyp4NDU5alRyKRw8fTg1Zx0yWTcZbhJ9PwQBMw4ZFnE+ZhcSLHpo","body":"#buugi !&QmX9YyULckT8PdTTQbYnFv9VbEms7dDdDF8X75TKdi5RBJ  !|LRINT^~A?GX9_NtQkqXS%2s.NHt7","nick":"samuimac","likes":16005,"shares":47616,"moderate":false,"type":"post","ptime":1639695952695},{"key":"x3hFNzhFVGtbeKMGSwX2XyAsQJtOtM34gNZRNDp2Z0A0GXWfR2lWVCQjJxZoYWh2Xwc4Px0APiEdSRcCORAHA2slDkojV2VFVCFOCiQ7KCoaHBlxaUsAcWdFJCgVRT8=","body":"#file:///home/alex/Bureau/test-40mm-macro/DSC_3975.jpg","nick":"ladislas","likes":0,"shares":0,"moderate":false,"type":"post","ptime":1639693852695},{"key":"11dLdW1/dlFtO6LUjgSYm9IU3Na6XdOt+rU8ODBEUHNOGHBLNJQxIo8cZ0/ujmJfwDAEDS0cdj8QFStOADk/Ni0uPwspBhBmWUsVHGN3HiADeB0nPzk8MB0JHjIjbGQdAgUIbycYKyMPNnRdKiBbFTULcCIpODMseG1pDH45NRgOe0x9dwM5AWMWI0g8fBMaOxEYfDlmHyFT","body":"new with attachement !&QmZ2YeeKVfhzqVjzTFp94s6siVDkMSNa9nau1WcACadpjv  !|LCD[Y,?1?1xc$.s=R$a|xbR$R$NE","nick":"majorllama","likes":12216,"shares":0,"moderate":false,"type":"post","ptime":1639687612695},{"key":"xTliU1w7cnJDcho4VJkKXzKmWqSfszPRYQJEeEU3M0xyKzWneW9oURskBj4CRCcmHCpkXicUfhcVPF03JkU1MAATICUz","body":"#buugi this is Martins sandbox","nick":"guest","likes":0,"shares":0,"moderate":false,"type":"post","ptime":1639687252695},{"key":"5DhWTWBuW0qzeVM4G4RI43ncMqCdvdJZNQVVs3RVem1yYjlzRVdEJSFvFAJbERkSHVIbNBYpSwY8IQU=","body":"#finhack beep boop","nick":"soup","likes":0,"shares":0,"moderate":false,"type":"post","ptime":1639681093393},{"key":"5XNKSnpOEWKqU85VjkKG37k61+LJMB3ZgRTHT7cgO0VxRzUrTDJpIm4WFAFSDjgHMQQRJ24ULTBWN1xWGwImQA==","body":"new without attachement","nick":"guest","likes":0,"shares":0,"moderate":false,"type":"post","ptime":-1},{"key":"0G5HWTxhZFrLYB9FKGe42lSS36I1tKRwM7j+CiBkcydM2txdtBxIWDeCOIWcwHXmKxFFHFsRJBlfFkNtOh0qFQQaEi4RHCgSDlwYLzwaJwMsHjUZDS8pAicYbyMPI1cCDRkGABheAhcpIHN1cRJ4ODFTJyYNMmxsIlU/Yj0AIn1cJTM5MhgPDy4gEg==","body":"priisti !&QmRCfyZEGTQsM2ybwMiEvxyqiiQociVfHf11FXQCU8udcT  !|LNPjDUkB-;oft7WBt7of~qkBWBfP","nick":"guest","likes":52907,"shares":15294,"moderate":false,"type":"post","ptime":-1},{"key":"xhdUNDh/VUg0WC7bPrwVyTqnFpWIz83mJP5XWDJh0kRlZlX2yY9nuOS7RzeVpL7f4QgnDIb8meFHNS9XWFhKPz4CPSux7BVPFCk6OkxcUCMeAQw4ChM8HzExTFgbWSMDGy8FNT8fGVEsExYnLgZOcHEWH0IqGG4=","body":"käärmeen sylissä !&QmWzj2huqtnhpttgy59X7BNPxKseyU9HUnJjww56SyqaY9","nick":"guest","likes":11278,"shares":49886,"moderate":false,"type":"post","ptime":-1},{"key":"0G5HWTxhZ14/YI0DkYXeSdGUEBh2RzXW0gBYVDjMa+dVEFpTNKJIvy27NFb81YNvBxBtTVANIgpfFkM3JzE9JiNYJw0bLCYvKhgvBTMhY31qKmklQQtdIGRRGGMWKAdSOCwOOSQXIx0TAWAXESVuDjtWO0U1HyMGJ38SElIVPAYKKj0DC2ktfwMZEg==","body":"#buugi !|LAEpA;ofMd_NivNHxv-;0L%M%M%M  !&QmaasmYziqTnYu3BAKZxZoX6SobQjLYG8WjL9ipKR3u2Ki","nick":"guest","likes":3344,"shares":44734,"moderate":false,"type":"post","ptime":-1},{"key":"5DhWTWBuW0qzeVM4dlJDM0BzV3IIsVSnGNYymJqO60JyHy/LR3hNJSFvFAJbERkSHVIbNBYpSwY8IQU=","body":"#finhack beep boop","nick":"guest","likes":0,"shares":0,"moderate":false,"type":"post","ptime":-1},{"key":"sWV4RTw6fXKSMd9513cBFwqYfnxRut5qB+iNhSobYDdBvt9N4AJTzXa4RmNVVm7ndRsJdSxTDSchAUYnAixVRwoIGiNcNg4fMkc8MU0aaxQCEBUdcEgIaywXWhkaUg==","body":"a valued rug !|LKHeatNG~pR*_3NG%MWB~pRjE1WB","nick":"guest","likes":9732,"shares":8352,"moderate":false,"type":"post","ptime":-1},{"key":"sWV4RTw6fXaSMdJ52ncBFwqYfnxRuit0VtEwmLrybjNBvt9N4AJTZHa4QUuFLZwWdRsJdSxTDSchAUYnAixVRwoIGiNsF0ofMkc8MU0aaxQCEBUdcEgIaywbPhkaUg==","body":"a valued rug !|LKHU@0NG~pR*_3NG%MWB~pRjIUWB","nick":"guest","likes":63103,"shares":53841,"moderate":false,"type":"post","ptime":-1},{"key":"yVNSTkxQBlisW3xgHEWIy0qQ8qEXkyoluHb8AaEmKwxq7cNA2wE3F0vIN/WiSpA1GQ8VKU8UU1h2S2dbNCwNBEoSCydxXwAROyAEHlQdPmMALi0XKjwBLjk7Jzlscwk3AWFyLgcyAVRXU2sIHxFnFgwvFQ9TQCsNbHkifw95OCkSZmQbYCYqdng=","body":"gazza !&QmVgft2DiD94VYBAGp5Pu4NhwqfTehAVcHU6NrgR9oPqL2  !|LD7x8Yt603M}-.oLD,RkD,WX-ns,","nick":"guest","likes":39692,"shares":49758,"moderate":false,"type":"post","ptime":-1},{"key":"8XJOR1I+WVXIM5b/eZTFgSS4lEZUOg8SoQ12EXw7zntnAVokRMyQnAeQTbT5bO70ACprVxtHHz5NFxAzJjNJIS83LysAAnApBRc6MC8SOQgwYzAuLQk1TwkAFX0Dfx1RBh8KMyp4NDU5alRyKRw8fTg1Zx0yWTcZbhJ9PwQBMw4ZFnE+ZhcSLHpo","body":"#buugi !&QmX9YyULckT8PdTTQbYnFv9VbEms7dDdDF8X75TKdi5RBJ  !|LRINT^~A?GX9_NtQkqXS%2s.NHt7","nick":"samuimac","likes":16005,"shares":47616,"moderate":false,"type":"post","ptime":-1},{"key":"+Ho0ZjNfZWuecBdLLEbg3Sewv5Jopo35ZzchiMCDTHZccC1qYnkzSncmWF0pHhIVDhkYGBAWJgA=","body":"conely wainbows","nick":"guest","likes":0,"shares":0,"moderate":false,"type":"post","ptime":-1},{"key":"xXJ6ej9sHlZNarwEdz3+SWoXRRHCwDeRpEBjS381eFtLNlXmbH1tSkYnDTdVWi0cS0YcFAcX","body":"suburbian selva","nick":"ladislas","likes":0,"shares":0,"moderate":false,"type":"reply","ptime":-1},{"key":"5DhWTWBuW0qzeVM4dlIu5Uujbt0AtaetrFdDNmCtcG16YjkzRThFJSFvFAJbERkSHVIbNBYpSwY8IQU=","body":"#finhack beep boop","nick":"guest","likes":0,"shares":0,"moderate":false,"type":"post","ptime":-1},{"key":"5DhWTWBuW0qzeVM4dlJDMy2lXKJcyDql2aPaZHYoZJVwYjEzRXhFJSFvFAJbERkSHVIbNBYpSwY8IQU=","body":"#finhack beep boop","nick":"guest","likes":0,"shares":0,"moderate":false,"type":"post","ptime":-1},{"key":"yVNSTkxQBmisW3lgGUWIy0qQ8qEXk/IcMZz7PZ5WNwxq5cdAWwE3BUvINFkBd8ooGQ8VKUcXAFh2S2dbMXwsNkkgBDUpMwUgDQ8IOis6fTkMcCMrDiVXFCwuCBZDJw89DwUOJDg2CFJXU2sIH2EBA3IFTAodMQk5KgwbDAIVMhYIJnwEPzsPKHg=","body":"gary2 !&QmS7GF1vfVaXShtnKTJw6nB6yMBM3RTCLgzbHxi6EeouE4  !|L4QmFs-3~BoIk[V?I@XT^lOGrsVr","nick":"guest","likes":14385,"shares":38979,"moderate":false,"type":"post","ptime":-1},{"key":"zGdZN2NcazKYU8hk6lSshcm3+fODuI+APUEVapj5cxVR0MMt2Q4wfnnA0WAmsXHXEm8MRxo1AV9YBWtKVik7ASAcDx4ATyQGBzQ/ExACATwXHgMmKAIpAzIAInQxEAAhOAMnNQITBB82GTZwThUKLQtVITYvLjF1TxwXAAs5L1h6GgoUIDk0JwhZOFE=","body":"bubbling !&QmcCTdHH6EEiUrXGLGfqRkBnzDGC9g3tv3jyTdxddwUBJc  !|L26RP_of8|WBjIoek9WBMzayoxkB","nick":"guest","likes":19967,"shares":10661,"moderate":false,"type":"post","ptime":-1}]`
 const jason = JSON.parse(dump)
 
 const messageUtilMixin = {
   methods: {
+    since(messageContainer) {
+      if (messageContainer.ptime <= 0) {
+        return "some time"
+      }
+      let d = new Date(messageContainer.ptime)
+      const msSince = new Date()-d
+      const SECOND = 1 * 1000
+      const MINUTE = SECOND * 60
+      const HOUR = MINUTE * 60
+      const DAY = HOUR * 24
+      const WEEK = DAY * 7
+      const MONTH = WEEK * 30
+      if (msSince < SECOND) return "less than a second"
+      if (msSince < WEEK || msSince < MONTH ) return `${Math.ceil(msSince / DAY)} days`
+      if (msSince > MONTH)  return "many days"
+      return "??? days"
+    },
     getUsableText(messageContainer) {
       const match = /(#\S+\s)?(.+)/g.exec(messageContainer.body)
       if (!match) return ""
@@ -43,7 +60,9 @@ Vue.component("channel-view", {
     <button @click="goBack" class="contrast">back</button>
     <h2>{{ channel }}</h2>
     <div class="category-container" v-for="message in messages">
-      <span>{{ message.nick }}</span> <span> {{ getUsableText(message) }} </span>
+      <span>{{ message.nick }}</span> 
+      <small> {{ since(message) }} old </small>
+      <span> {{ getUsableText(message) }} </span>
     </div>
   </div>
   `
@@ -110,23 +129,6 @@ Vue.component("base-view", {
       this.currentView = viewName
       this.focusedChannel = arg
     },
-    since(messageContainer) {
-      if (messageContainer.ptime <= 0) {
-        return "some time"
-      }
-      let d = new Date(messageContainer.ptime)
-      const msSince = new Date()-d
-      const SECOND = 1 * 1000
-      const MINUTE = SECOND * 60
-      const HOUR = MINUTE * 60
-      const DAY = HOUR * 24
-      const WEEK = DAY * 7
-      const MONTH = WEEK * 30
-      if (msSince < SECOND) return "less than a second"
-      if (msSince < WEEK || msSince < MONTH ) return `${Math.ceil(msSince / DAY)} days`
-      if (msSince > MONTH)  return "many days"
-      return "??? days"
-    }
   },
   template: `
   <div>
@@ -139,7 +141,7 @@ Vue.component("base-view", {
     </div>
     <channel-view v-model="currentView" :channel=focusedChannel :messages="getChannelMessages(focusedChannel)" v-if="currentView == 'channel'"></channel-view>
     <template v-if="currentView == 'overview'">
-    <h2>Categories</h2>
+    <h2>Topics</h2>
     <div class="categories-container">
       <div @click="changeView('channel', channel)" class="category-container" v-for="channel in this.getChannelNames()">
         <h3>{{ channel }}</h3>
